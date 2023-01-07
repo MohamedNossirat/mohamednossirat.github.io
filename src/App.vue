b8ab1d3ce914ea2f29d6f76e31835fdb68eaa55d
Mohamed Nossirat <78148222+MohamedNossirat@users.noreply.github.com>
2023-01-07 09:00:07 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+  <TheAppBar/>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import TheAppBar from '@/components/layouts/TheAppBar.vue'
+</script>
---
+++ src/components/layouts/TheAppBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app-bar app flat>
+        <v-row justify="space-between" align="center" class="pa-6">
+            <v-col class="d-flex justify-center">
+                <h3>Mohamed AL NOSSIRAT</h3>
+            </v-col>
+            <v-col class="d-flex justify-center">
+               <v-toolbar-items>
+                <v-btn variant="plain">Home</v-btn>
+                <v-btn variant="plain">Blog</v-btn>
+                <v-btn variant="plain">Projects</v-btn>
+                <v-btn variant="plain">gallery</v-btn>
+               </v-toolbar-items>
+            </v-col>
+            <v-col class="d-flex justify-end">
+                    <v-btn variant="plain" icon>
+                        <v-icon>
+                            mdi-white-balance-sunny
+                        </v-icon>
+                    </v-btn>
+            </v-col>
+        </v-row>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    name:'TheAppBar'
+}
+</script>
